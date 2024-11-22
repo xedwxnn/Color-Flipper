789b0076cef5eb0ead19919abd5b98c392b2d1e2
Edwxn <edwinpena0306@gmail.com>
2024-11-22 12:48:37 -0400

    arreglos + actualización

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,7 @@
-const body = document.getElementsByTagName("body")[0];
+const square = document.getElementById("square");
+const currentColorEl = document.getElementById('currentColor');
 
 function setColor(color) {
-    body.style.backgroundColor = color;
-}
-
-function setRandomColor() {
-    const colors = ['red', 'yellow', 'blue'];
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    body.style.backgroundColor = randomColor;
+    square.style.backgroundColor = color;
+    currentColorEl.innerText = color;
 }
